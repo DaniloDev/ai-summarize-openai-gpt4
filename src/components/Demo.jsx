2d9d13980d587b33ae4a2e8f859a3255e10d0751
Danilo Santos Nascimento <danisn2010@gmail.com>
2023-09-07 13:01:49 -0300

    feat: copy url to the clipboard

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -9,6 +9,7 @@ const Demo = () => {
     summary: ''
   })
   const [ allArticles, setAllArticles ] = useState([])
+  const [copied, setCopied] = useState('');
 
   const [ getSummary,{ error, isFetching } ] = useLazyGetSummaryQuery()
 
@@ -36,6 +37,12 @@ const Demo = () => {
 
       localStorage.setItem('articles', JSON.stringify(updateAllArticles))
     }
+  }
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl)
+    navigator.clipboard.writeText(copyUrl)
+    setTimeout(() => setCopied(false), 3000)
   }
 
   return (
@@ -76,9 +83,9 @@ const Demo = () => {
               onClick={() => setArticle(item)}
               className='link_card'
             >
-              <div className='copy_btn'>
+              <div className='copy_btn' onClick={() => handleCopy(item.url)}>
                 <img 
-                  src={copy}
+                  src={copied === item.url ? tick : copy}
                   alt='copy_icon'
                   className='w-[40%] h-[40%] object-contain'
                 />
